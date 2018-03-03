907e662283c4ab30d93148ca8bc2a6fb2d2239c7
kslisenko <kslisenko@gmail.com>
2018-03-03 14:41:22 +0300

    Added web-socket + spring example

--- app/src/main/java/com/example/demo/DemoApplication.java
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,61 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.time.LocalTime;
+
+@Configuration
+@EnableWebSocket
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebSocketConfigurer {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry r) {
+        r.addHandler(new MyHandler(), "/websocket");
+    }
+
+    static class MyHandler extends TextWebSocketHandler {
+
+		@Override
+		public void afterConnectionEstablished(WebSocketSession s) throws Exception {
+            log.info("WS connection established {}", s.getId());
+		}
+
+		@Override
+		protected void handleTextMessage(WebSocketSession s, TextMessage m) throws Exception {
+            log.info("WS ({}) {}", s.getId(), m);
+            s.sendMessage(new TextMessage("responce " + LocalTime.now()));
+		}
+
+		@Override
+		protected void handlePongMessage(WebSocketSession s, PongMessage m) throws Exception {
+            log.info("WS PONG ({}) {}", s.getId(), m);
+		}
+
+		@Override
+		public void handleTransportError(WebSocketSession s, Throwable e) throws Exception {
+            log.error("WS error (" + s.getId() + ")", e);
+		}
+
+		@Override
+		public void afterConnectionClosed(WebSocketSession s, CloseStatus st) throws Exception {
+            log.info("WS connection closed {}, status {}", s.getId(), st);
+		}
 	}
 }
